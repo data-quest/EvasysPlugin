4f96ff633a94c723507af5af61959ab9271502d1
Rasmus Fuhse <krassmus@gmail.com>
2020-03-27 15:52:18 +0100

    prepare for 8.0 api

--- views/passiveaccount/index.php
+++ views/passiveaccount/index.php
@@ -1,9 +1,24 @@
-<iframe style="width:100%; height: 90vh; border: none;"
-        src="<?= htmlReady($link['ServerIp'] . $link['UserStartPage'] . "&PHPSESSID=" . urlencode($link['SessionId'])) ?>"></iframe>
-
+<? /*var_dump($link)*/ ?>
 
 <style>
     #layout-sidebar {
         display: none;
     }
 </style>
+
+<? if ($link['Token']) : ?>
+    <form method="POST"
+          action="<?= htmlReady($link['ServerIp'] . "/public/forward") ?>"
+          class="default"
+          target="_blank">
+        <input type="hidden" name="forwardinfo" value="<?= htmlReady($link['Token']) ?>">
+
+        <div style="text-align: center">
+            <?= \Studip\Button::create(_("Nach EvaSys (öffnet in neuem Reiter)")) ?>
+        </div>
+
+    </form>
+<? else : ?>
+    <iframe style="width:100%; height: 90vh; border: none;"
+            src="<?= htmlReady($link['ServerIp'] . $link['UserStartPage'] . "&PHPSESSID=" . urlencode($link['SessionId'])) ?>"></iframe>
+<? endif ?>
